a6fec631d66cf219d444389c0315aa89111b2acb
David Ilie <david@davidilie.com>
2021-10-01 12:15:15 +0200

    (feat): added seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 
 import AppLayout from "../components/AppLayout";
@@ -8,11 +9,23 @@ import "tailwindcss/tailwind.css";
 
 const LandingPage = ({ Component, pageProps }: AppProps) => {
     return (
-        <ThemeProvider defaultTheme="dark">
-            <AppLayout>
-                <Component {...pageProps} />
-            </AppLayout>
-        </ThemeProvider>
+        <>
+            <DefaultSeo
+                defaultTitle="David Ilie Apps Platform"
+                openGraph={{
+                    title: "David Ilie Apps Platform",
+                    type: `website`,
+                    site_name: "David Ilie Apps Platform",
+                    description: "Landing page used for unused domains.",
+                }}
+                description="Landing page used for unused domains."
+            />
+            <ThemeProvider defaultTheme="dark">
+                <AppLayout>
+                    <Component {...pageProps} />
+                </AppLayout>
+            </ThemeProvider>
+        </>
     );
 };
 
