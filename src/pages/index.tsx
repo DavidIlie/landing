b9005cbd8a119b46c73abef0bfcebf3bf1dd706f
David Ilie <david@davidilie.com>
2021-10-04 16:40:28 +0200

    added legal stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,41 @@
+import { Fade } from "react-awesome-reveal";
+
 const Home = (): JSX.Element => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <h1 className="text-gradient lg:text-6xl sm:text-5xl text-4xl font-semibold">
-                David Ilie Apps Platform
-            </h1>
-            <div className="flex gap-4 mt-1">
-                <h1 className="text-xl text-gray-400">
-                    A project by{" "}
-                    <a
-                        href="https://davidilie.com"
-                        target="_blank"
-                        className="text-blue-400 hover:text-blue-500 duration-150"
-                    >
-                        David Ilie
-                    </a>
+            <Fade direction="up" triggerOnce cascade>
+                <h1 className="text-gradient lg:text-6xl sm:text-5xl text-4xl font-semibold">
+                    David Ilie Apps Platform
                 </h1>
+                <div className="flex gap-4 mt-2">
+                    <h1 className="text-xl text-gray-400">
+                        A project by{" "}
+                        <a
+                            href="https://davidilie.com"
+                            target="_blank"
+                            className="text-blue-400 hover:text-blue-500 duration-150"
+                        >
+                            David Ilie
+                        </a>
+                    </h1>
+                </div>
+            </Fade>
+            <div className="bottom-0 fixed px-5 py-5 w-full flex justify-center items-center text-xl">
+                <div>
+                    <a
+                        href="/privacy-policy.html"
+                        className="text-blue-500 hover:text-blue-600 duration-150"
+                    >
+                        Privacy Policy
+                    </a>
+                    ,{" "}
+                    <a
+                        href="/terms-of-service.html"
+                        className="text-blue-500 hover:text-blue-600 duration-150"
+                    >
+                        Terms of service
+                    </a>
+                </div>
             </div>
         </div>
     );
